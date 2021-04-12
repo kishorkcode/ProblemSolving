93c86ef9b2a1ca5b1bd5be2f12f28c4263e1be7f
Kishor <kishor4kumar@yahoo.com>
2021-04-12 23:23:40 +0530

    Connected Cells - DFS

---
+++ CSharp/ConnectedCell.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class ConnectedCell
+    {
+        private int n, m;
+        private int[][] matrix;
+        private int result;
+
+        public void GetInputs()
+        {
+            n = Convert.ToInt32(Console.ReadLine());
+            m = Convert.ToInt32(Console.ReadLine());
+
+            matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                var row = Console.ReadLine();
+                matrix[i] = Array.ConvertAll(row.Split(' '), x => Convert.ToInt32(x));
+            }
+        }
+
+        public void PrintOutput()
+        {
+            Console.WriteLine(result);
+        }
+
+        public void Perform()
+        {
+
+        }
+
+    }
+}
